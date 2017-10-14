56d64f3bfcaa626e110a08db09628a1c619b5913
Hadesnow <snowbloom1991@gmail.com>
2017-10-14 18:39:28 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,88 @@
 var colorList=[
-  '#f39e52',
-  '#f0d589',
-  '#dc647f',
-  '#6fa3d7',
-  '#1386ad',
-  '#3d5575',
-  '#6aad68',
-  '#997aa3',
-  '#fbe2e5',
-  '#3162ad'
+  '#1E3140',
+  '#19515C',
+  '#10736F',
+  '#2C9677',
+  '#60B773',
+  '#A1D668',
+  '#EEEF61'
   ]
-
+var i = 9;
 function setup() {
-  createCanvas(300,440);
-  
-  frameRate(1);
-  
+  createCanvas(1000,1000);
+  frameRate(4);
 }
 
 function draw(){
-  noStroke();
-  
-  for(var x=0; x<width; x+=50 ){
-    for(var y =0; y<=height*1.1; y+=40){
-      quad(x,y,x+50,y-60,x+50,y-20,x,y+40);
+  translate(-16*i, -16*i);
+  strokeWeight(1);
+  stroke(color(0,0,0,40));
+  for(var x=0; x<width*2; x+=16*i ){
+    for(var y =0; y<=height*2; y+=16*i){
+        beginShape()
+        vertex(7*i+x,0+y);
+        vertex(9*i+x,1*i+y);
+        vertex(10*i+x,4*i+y);
+        vertex(9*i+x,6*i+y);
+        vertex(10*i+x,10*i+y);
+        vertex(11*i+x,11*i+y);
+        vertex(12*i+x,9*i+y);
+        vertex(11*i+x,9*i+y);
+        vertex(11*i+x,7*i+y);
+        vertex(13*i+x,6*i+y);
+        vertex(13*i+x,11*i+y);
+        vertex(15*i+x,12*i+y);
+        vertex(16*i+x,10*i+y);
+        vertex(15*i+x,10*i+y);
+        vertex(15*i+x,7*i+y);
+        vertex(17*i+x,4*i+y);
+        vertex(17*i+x,6*i+y);
+        vertex(19*i+x,5*i+y);
+        vertex(22*i+x,5*i+y);
+        vertex(20*i+x,10*i+y);
+        vertex(17*i+x,12*i+y);
+        vertex(16*i+x,14*i+y);
+        vertex(18*i+x,15*i+y);
+        vertex(19*i+x,14*i+y);
+        vertex(18*i+x,14*i+y);
+        vertex(19*i+x,13*i+y);
+        vertex(20*i+x,14*i+y);
+        vertex(17*i+x,18*i+y);
+        vertex(17*i+x,20*i+y);
+        vertex(15*i+x,23*i+y);
+        vertex(15*i+x,26*i+y);
+        vertex(16*i+x,26*i+y);
+        vertex(15*i+x,28*i+y);
+        vertex(13*i+x,27*i+y);
+        vertex(13*i+x,22*i+y);
+        vertex(11*i+x,23*i+y);
+        vertex(11*i+x,25*i+y);
+        vertex(12*i+x,25*i+y);
+        vertex(11*i+x,27*i+y);
+        vertex(10*i+x,26*i+y);
+        vertex(9*i+x,22*i+y);
+        vertex(10*i+x,20*i+y);
+        vertex(9*i+x,17*i+y);
+        vertex(7*i+x,16*i+y);
+        vertex(7*i+x,17*i+y);
+        vertex(2*i+x,19*i+y);
+        vertex(3*i+x,21*i+y);
+        vertex(1*i+x,22*i+y);
+        vertex(1*i+x,18*i+y);
+        vertex(4*i+x,14*i+y);
+        vertex(3*i+x,13*i+y);
+        vertex(2*i+x,14*i+y);
+        vertex(3*i+x,14*i+y);
+        vertex(2*i+x,15*i+y);
+        vertex(0+x,14*i+y);
+        vertex(1*i+x,12*i+y);
+        vertex(4*i+x,10*i+y);
+        vertex(6*i+x,5*i+y);
+        vertex(3*i+x,5*i+y);
+        vertex(2*i+x,3*i+y);
+        vertex(7*i+x,1*i+y);
+        endShape(CLOSE);
       fill(color(random(colorList)));
     }
   }
-  
-  if(frameCount==10){
-    noLoop();
-  }
-  
 }
-
